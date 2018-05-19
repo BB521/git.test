4bdb2d08a8ee7b77588f0a91c525a9efdf3a883e
905218080 <905218080@qq.com>
2018-05-19 10:03:59 +0800

    初始化项目

---
+++ gitTest/gitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitTest
+//
+//  Created by Binbin Mu on 2018/5/19.
+//  Copyright © 2018年 Binbin Mu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
